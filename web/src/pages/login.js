8848bb869aa8262c3828af1c70d498e775bf2ecd
Travers Geoffray <traversgeoffray@gmail.com>
2023-01-26 11:27:16 -0600

    built client and login js

---
+++ web/src/pages/login.js
@@ -0,0 +1,73 @@
+import FiveLiftsClient from '../api/fiveLiftsClient';
+// import Header from '../components/header';
+import BindingClass from '../util/bindingClass';
+import DataStore from '../util/DataStore';
+
+/**
+ * Logic needed for the create playlist page of the website.
+ */
+class Login extends BindingClass {
+    constructor() {
+        super();
+        this.bindClassMethods(['mount', 'submit', 'redirectToViewPlaylist'], this);
+        this.dataStore = new DataStore();
+        this.dataStore.addChangeListener(this.redirectToViewPlaylist);
+        // this.header = new Header(this.dataStore);
+    }
+
+    /**
+     * Add the header to the page and load the FiveLiftsClient.
+     */
+    mount() {
+        document.getElementById('login').addEventListener('click', this.login);
+        document.getElementById('create-profile').addEventListener('click', this.createProfile)
+
+        // this.header.addHeaderToPage();
+
+        this.client = new MusicPlaylistClient();
+    }
+
+    /**
+     * Method to run when the login button is pressed. Call the FiveLiftsService to login
+     * the user.
+     */
+    async login() {
+        this.client.login;
+        this.redirectToWorkout;
+    }
+
+    /**
+     * When the user has logged in, redirect to their workout page.
+     */
+    async redirectToWorkout() {
+        // TODO
+        window.alert("Redirect to workout page.")
+    }
+
+    /**
+     * Method to run when the create profile button is pressed. Call the FiveLiftsService to login
+     * the user.
+     */
+    async createProfile() {
+        this.client.login;
+        this.redirectToCreateProfile;
+    }
+
+    /**
+     * When the user has created a login information, redirect to create profile page.
+     */
+    async redirectToCreateProfile() {
+        // TODO
+        window.alert("Redirect to create profile page.")
+    }
+}
+
+/**
+ * Main method to run when the page contents have loaded.
+ */
+const main = async () => {
+    const login = new Login();
+    login.mount();
+};
+
+window.addEventListener('DOMContentLoaded', main);
--- web/webpack.config.js
+++ web/webpack.config.js
@@ -23,9 +23,7 @@ module.exports = {
     usedExports: true
   },
   entry: {
-    createPlaylist: path.resolve(__dirname, 'src', 'pages', 'createPlaylist.js'),
-    viewPlaylist: path.resolve(__dirname, 'src', 'pages', 'viewPlaylist.js'),
-    searchPlaylists: path.resolve(__dirname, 'src', 'pages', 'searchPlaylists.js'),
+    login: path.resolve(__dirname, 'src', 'pages', 'login.js')
   },
   output: {
     path: path.resolve(__dirname, 'build', 'assets'),
