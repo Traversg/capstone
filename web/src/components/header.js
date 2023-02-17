3561870c81ac4be3b31e04c282a5991cf50de58c
Travers Geoffray <traversgeoffray@gmail.com>
2023-02-17 16:04:13 -0600

    almost done with mobile menu

--- web/src/components/header.js
+++ web/src/components/header.js
@@ -19,15 +19,13 @@ export default class Header extends BindingClass {
      * Add the header to the page.
      */
     async addHeaderToPage() {
-        const currentUser = await this.client.getIdentity();
-
         const siteTitle = this.createSiteTitle();
-        // const userInfo = this.createUserInfoForHeader(currentUser);
 
         const header = document.getElementById('header');
         header.appendChild(siteTitle);
         const navBar = document.createElement('nav');
         navBar.innerHTML = `
+        <div class="fullNav">
         <ul>
             <li class="dropdown">
                 <button class="dropbtn">Progress</button>
@@ -42,10 +40,16 @@ export default class Header extends BindingClass {
             <li><a href="/upcomingWorkouts.html">Upcoming Workouts</a></li>
             <li><a href="/workoutHistory.html">Workout History</a></li>
             <li><button class="logoutButton" id="logout" type="button">Logout</button></li>
-        </ul>`;
+        </ul>
+        </div>
+        <div class="hamburgerNav">
+            <button type="button" class="hamburgerButton" id="hamburgerButton">&#9776</button>
+        </div>
+        `;
         header.appendChild(navBar);
-        // header.appendChild(userInfo);
+
         document.getElementById('logout').addEventListener('click', this.client.logout);
+        document.getElementById('hamburgerButton').addEventListener('click', this.displayMobileMenu);
     }
 
     createSiteTitle() {
@@ -61,39 +65,31 @@ export default class Header extends BindingClass {
         return siteTitle;
     }
 
-
-/*    createUserInfoForHeader(currentUser) {
-        const userInfo = document.createElement('div');
-        userInfo.classList.add('user');
-
-        const childContent = currentUser
-            ? this.createLogoutButton(currentUser)
-            : this.createLoginButton();
-
-        userInfo.appendChild(childContent);
-
-        return userInfo;
+    displayMobileMenu() {
+        document.getElementById('background').style.backgroundColor = "#ff0024";
+        document.getElementById('hamburgerButton').innerText = "x";
+        document.getElementById('workoutHistoryCard').innerHTML = `
+        <div class="mobileMenu" id="mobileMenu">
+        <div class="dropdown">
+            <button class="dropbtn">Progress</button>
+            <div class="dropdown-content">
+                <a href="/squatProgress.html">Squat</a>
+                <a href=/benchPressProgress.html">Bench Press</a>
+                <a href="/barbellRowProgress.html">Barbell Row</a>
+                <a href="/overheadPressProgress.html">Overhead Press</a>
+                <a href="/deadliftProgress.html">Deadlift</a>
+            </div>
+        </div>    
+        <a href="/upcomingWorkouts.html">Upcoming Workouts</a>
+        <a href="/workoutHistory.html">Workout History</a>
+        <button class="logoutButtonMobile" id="logout" type="button">Logout</button>
+        </div>
+    `;
+    document.getElementById('hamburgerButton').addEventListener('click', this.closeMobileMenu);
     }
 
-    createLoginButton() {
-        return this.createButton('Login', this.client.login);
+    closeMobileMenu() {
+        const mobileMenu = document.getElementById('mobileMenu');
+        mobileMenu.classList.add('hidden');
     }
-
-    createLogoutButton(currentUser) {
-        return this.createButton(`Logout: ${currentUser.name}`, this.client.logout);
-    }
-
-    createButton(text, clickHandler) {
-        const button = document.createElement('a');
-        button.classList.add('button');
-        button.href = '#';
-        button.innerText = text;
-
-        button.addEventListener('click', async () => {
-            await clickHandler();
-        });
-
-        return button;
-    }
-    */
 }
